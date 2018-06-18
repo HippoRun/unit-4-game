8163fa35013f96ddcb3625ce40708517afb3b40b
Mayra Lopez <hipporun@Mayras-MacBook-Pro.local>
2018-06-17 22:34:58 -0600

    out of time. At least got full background to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,60 @@
-// player will get a random number. 
+$(document).ready(function() {
+    //set global variables
+    var computerValue = 0;
+    var playerValue = 0;
+    var wins = 0;
+    var loss = 0;
 
-var player = 0;
+    $("#rules").click(function() {
+        $("#rules-expanded").slideToggle('slow');
+    })
 
-function playernumberInt (){
-    return parseInt(Math.random()* (120-19)+19);
-}
+    var reset = function() {
+        computerGuess();
+        getCrystalValues();
+        playerValue = 0;
+    }
 
-console.log(playernumberInt());
-player = playernumberInt();
+    $("#wins").text(" " + wins);
+    $("#losses").text(" " + loss);
 
-console.log(player);
+    computerGuess = function() {
+        guess = Math.floor(Math.random() * 120) + 19;
+        computerValue = guess;
+        $("#computer-guess").text(computerValue);
 
-// random for crystals
+    }
 
-function crystals () {
-    return parseInt(Math.random()*(12-1)+1);
-}
+    computerGuess();
+    console.log(computerValue);
+
+    getCrystalValues = function() {
+        for (i = 1; i <= 4; i++) {
+            crystalValue = Math.floor(Math.random() * 12) + 1;
+
+            $("#gem-" + i).attr("value", crystalValue);
+            console.log(crystalValue);
+        }
+    }
+    getCrystalValues();
+
+    $(".gems").on("click", function() {
+        stringValue = $(this).attr("value");
+
+        playerValue += parseInt(stringValue, 10);
+
+
+        if (playerValue === computerValue) {
+            wins++;
+            $("#wins").text(" " + wins);
+            reset();
+
+        } else if (playerValue > computerValue) {
+            loss++;
+            $("#losses").text(" " + loss);
+            reset();
+        }
+        $("#animalNum").text(playerValue);
+
+    })
+});
